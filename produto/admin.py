630e12e72f83d4c8fa24b0a885bafc9c3c12ff6d
Lucas Brino <81391577+lucbrino@users.noreply.github.com>
2023-05-10 14:47:32 -0300

    Add files via upload

---
+++ produto/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Produto, Categoria, Opcoes, Adicional
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('icone', 'nome_produto', 'categoria', 'preco', 'ativo')
+    list_editable = ('preco','ativo')
+
+admin.site.register(Categoria)
+admin.site.register(Opcoes)
+admin.site.register(Adicional)
